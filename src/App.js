7a28a6225344ed64995a62805b15a2eda18d2a37
mouadh arout <mouadharoutpro@gmail.com>
2025-02-09 17:20:58 +0100

    modify app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. azd
+          Edit <code>src/App.js</code> and save to reload. azdzefez
         </p>
         <a
           className="App-link"
